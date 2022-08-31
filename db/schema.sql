0065026bbf37360d5ec1de18059fb581916af724
Ryan Raposo <raposo.ryan@gmail.com>
2022-08-31 14:54:42 -0400

    add roles table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,16 @@
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+
 
 CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,16 @@ VALUES
   ('Sales'),
   ('Admin'),
   ('Manufacturing');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+   ('Junior Sales Representative', '50000', 1),
+   ('Senior Sales Representative', '80000', 1),
+   ('Reception', '35000', 2),
+   ('Purchaser', '50000', 2),
+   ('Operator', '30000', 3),
+   ('Team Leader', '40000', 3);
+
+
+
+
