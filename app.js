0fc8c98a1b538b44d7954872ae2f9d08cbdd9907
Ryan Raposo <raposo.ryan@gmail.com>
2022-08-31 16:50:24 -0400

    add the ability to add departments, roles, employees

--- app.js
+++ app.js
@@ -33,6 +33,19 @@ function viewAllDepartments() {
     });
 };
 
+function addDepartment(name) {
+    const sql = `INSERT INTO departments (name) VALUES (?)`;
+    const params = [name];
+
+    db.query(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        console.log(`Success! Added department: ${name}`);
+    });
+};
+
 // Roles
 function viewAllRoles() {
     const sql = `SELECT * FROM roles;`
@@ -42,6 +55,19 @@ function viewAllRoles() {
             return;
         }
         console.table(rows);
+    });
+};
+
+function addRole(title, salary, department_id) {
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+    const params = [title, salary, department_id];
+
+    db.query(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        console.log(`Success! Added role: ${title}`);
     });
 };
 
@@ -57,8 +83,24 @@ function viewAllEmployees() {
     });
 };
 
+function addEmployee(first_name, last_name, role_id, manager_id) {
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    const params = [first_name, last_name, role_id, manager_id];
+
+    db.query(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        console.log(`Success! Added employee: ${first_name} ${last_name}`);
+    });
+};
 
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
 });
+
+viewAllRoles();
+addRole('Hottest in the office', 1000000, 2);
+viewAllRoles();
