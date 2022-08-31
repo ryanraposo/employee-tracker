83e4d110a6300a97f4571bc8b2552fa9e2a92b5f
Ryan Raposo <raposo.ryan@gmail.com>
2022-08-31 14:02:46 -0400

    add prompt function

---
+++ app.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+
+function promptAction() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        },
+}
